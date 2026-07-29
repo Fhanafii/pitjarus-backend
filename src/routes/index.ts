7514f4900bc1a68d41d31b382295647369ad8d3d
Fhanafii <fhanafi544@gmail.com>
2026-07-29 20:27:58 +0700

    feat: adding store routes into index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { successResponse } from "../utils/response";
 import authRoute from "../modules/auth/auth.route";
+import storeRoutes from "../modules/stores/store.route";
 
 const router = Router();
 
 router.use(authRoute);
+router.use("/stores", storeRoutes);
 
 router.get("/health", (req, res) => {
   return successResponse(
