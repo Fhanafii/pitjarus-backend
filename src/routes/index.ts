26f39fbd8b09f51f545cc58a10f5e90865124720
Fhanafii <fhanafi544@gmail.com>
2026-07-30 17:16:31 +0700

    feat: adding new report modules route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,14 @@ import { successResponse } from "../utils/response";
 import authRoute from "../modules/auth/auth.route";
 import storeRoutes from "../modules/stores/store.route";
 import productRoutes from "../modules/products/product.route";
+import reportRoutes from "../modules/reports/report.route";
 
 const router = Router();
 
 router.use(authRoute);
 router.use("/stores", storeRoutes);
 router.use("/products", productRoutes);
+router.use("/report", reportRoutes);
 
 router.get("/health", (req, res) => {
   return successResponse(
