bc99086027d5cb5b20182a42352e64ae53a8ec8d
Fhanafii <fhanafi544@gmail.com>
2026-07-30 22:31:20 +0700

    feat: adding new routes for promo API

--- src/modules/reports/report.route.ts
+++ src/modules/reports/report.route.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 
 import attendanceRoute from "./attendance/attendance.route";
 import productReportRoute from "./product/productReport.route";
+import promoReportRoute from "./promo/promo.route";
 
 const router = Router();
 
 router.use(attendanceRoute);
 router.use(productReportRoute);
 
-// router.use(promoReportRoute);
+router.use(promoReportRoute);
 
 export default router;
